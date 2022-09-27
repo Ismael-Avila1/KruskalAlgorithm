3e9644e7ff85eec4844ec796dd5398dc53c4729e
Ismael Avila <ismael.guada234015@gmail.com>
2022-09-26 23:59:30 -0500

    Definición de la clase Graph

---
+++ Graph.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KruskalAlgorithm
+{
+    internal class Graph
+    {
+        Vertex[,] vertices;
+
+        public Graph() { }
+
+        public Graph(int size)
+        {
+            vertices = new Vertex[size, size];
+        }
+
+       
+
+
+
+    }
+}
