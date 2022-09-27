9a27920658ae93ac56e580a8db45687fb51750f7
Ismael Avila <ismael.guada234015@gmail.com>
2022-09-27 01:08:54 -0500

    Se un atributo de tipo Graph y dos Bitmap. El constructor inicializa el grafo, uno de los bitmap y los asigna al pictureBox. Se agregó el evento click en el botón buttonCreateVertices, el cual prepara al picturBox para que se puedan agregar los vértices. También se agregó el evento mouseClick al pictureBox para agregar un vértice donde el usuario haga click; este método valida si aún se pueden agregar vértices, si es así, se añade el vértice al grafo, se dibuja en el picturBox y se indica la cantidad restante de vértices por poner en un label.

--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,50 @@ namespace KruskalAlgorithm
 {
     public partial class Form1 : Form
     {
+        Graph graph;
+
+        Bitmap bmpOriginal;
+        Bitmap bmpGraph;
+
+
         public Form1()
         {
             InitializeComponent();
+            bmpOriginal = new Bitmap(pictureBox.Width, pictureBox.Height);
+            pictureBox.Image = bmpOriginal;
+            
+            graph = new Graph();
+        }
+
+        private void buttonCreateVertices_Click(object sender, EventArgs e)
+        {
+            labelRemainingVertices.Text = "Selecciona dónde quieres poner los " + numericUpDownVerticesNumber.Value + " vértices en el lienzo de arriba!!";
+            labelRemainingVertices.ForeColor = Color.Black;
+            pictureBox.Enabled = true;
+            pictureBox.BackColor = Color.White;
+
+            groupBox1.Enabled = false;
+            groupBox2.Enabled = true;
+        }
+
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(graph.VertexCount == (int)numericUpDownVerticesNumber.Value) {
+                labelRemainingVertices.Text = "Ya no quedan vértices por agregar";
+                labelRemainingVertices.ForeColor = Color.DarkRed;
+                return;
+            }
+
+            graph.addVertex(e.X, e.Y);
+
+            Graphics g = Graphics.FromImage(bmpOriginal);
+            Brush b = new SolidBrush(Color.Black);
+            
+            g.FillEllipse(b, e.X - 20, e.Y - 20, 50, 50);
+
+            pictureBox.Refresh();
+            labelRemainingVertices.Text = "Quedan " + (numericUpDownVerticesNumber.Value - graph.VertexCount) + " por agregar";
+            labelRemainingVertices.ForeColor = Color.DarkGreen;
         }
     }
 }
