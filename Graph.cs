9e8f437e2a3dbf69aad11d5a8f5b647b3905d654
Ismael Avila <ismael.guada234015@gmail.com>
2022-09-27 20:29:33 -0500

    Algoritmo de Kruskal terminado. Retorna la lista de aristas que conforman el árbol de recubrimiento mínimo

--- Graph.cs
+++ Graph.cs
@@ -38,9 +38,22 @@ namespace KruskalAlgorithm
             List<Edge> candidates = getCandidates();
             List<Edge> promising = new List<Edge>();
 
+            Edge e_i;
+            int index1, index2;
+
             int[,] CC = ccInitialize();
 
+            while(candidates.Count != 0) {
+                e_i = selection(candidates);
 
+                index1 = findInCC(CC, e_i.Origin);
+                index2 = findInCC(CC, e_i.Destination);
+
+                if(index1 != index2) {
+                    combineCC(CC, index1, index2);
+                    promising.Add(e_i);
+                }
+            }
 
             return promising;
         }
@@ -70,6 +83,38 @@ namespace KruskalAlgorithm
                 cc[i, i] = vertices[i].Id;
 
             return cc;
+        }
+
+        Edge selection(List<Edge> candidates)
+        {
+            Edge candidate; 
+            int minorIndex = 0;
+
+            for(int i=0; i<candidates.Count; i++)
+                if(candidates[i].Weight < candidates[minorIndex].Weight)
+                    minorIndex = i;
+            
+            candidate = candidates[minorIndex];
+            candidates.RemoveAt(minorIndex);
+            return candidate;
+        }
+
+        int findInCC(int[,] CC, Vertex v_d)
+        {
+            for (int i = 0; i < VertexCount; i++)
+                for (int j = 0; j < VertexCount; j++)
+                    if (CC[i, j] == v_d.Id)
+                        return i;
+            return -1;
+        }
+
+        void combineCC(int[,] CC, int index1, int index2)
+        {
+            for(int i=0; i<VertexCount; i++)
+                if(CC[index2, i] != -1) {
+                    CC[index1, i] = CC[index2, i];
+                    CC[index2, i] = -1;
+                }
         }
 
 
